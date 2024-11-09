dfe202bbbd1e27a7eacbadeefc92114b7af4152e
samghoreyshi <samghoreyshi@outlook.com>
2024-11-10 02:06:06 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+    webpack: (config) => {
+      config.cache = false; // Disable Webpack caching
+      return config;
+    },
+};
